5c64a416eeca2acc423bacbe4f5b48497d262649
Sushpal <nenavathsushpal4@gmail.com>
2026-06-10 15:06:29 +0530

    Remove email debug logs

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -36,8 +36,7 @@ const sendEmail = async (to, subject, text, html) => {
       html, // html body
     });
 
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  
   } catch (error) {
     console.error('Error sending email:', error);
   }
