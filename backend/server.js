1be679c2b567cfb896701a39d11c6b67548ae0c7
Sushpal <nenavathsushpal4@gmail.com>
2026-06-29 15:19:42 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@
   const connectDB = require('./src/config/db')
 
 
-    connectDB()
+  connectDB()
 
-   
 
-  app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
-  })
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
 
--- backend/src/controllers/account.controller.js
+++ backend/src/controllers/account.controller.js
@@ -1,22 +1,39 @@
 const accountModel = require("../models/account.model")
 const generateAccountNumber = require("../utils/generateAccountNumber");
 
-async function createAccountController(req,res){
+async function createAccountController(req, res) {
 
-    const user=req.user
+    const user = req.user
+
+    let accountNumber;
+    let isUnique = false;
+    let attempts = 0;
+    const MAX_ATTEMPTS = 5;
+
+    while (!isUnique && attempts < MAX_ATTEMPTS) {
+        accountNumber = generateAccountNumber()
+        const exists = await accountModel.findOne({ accountNumber })
+        if (!exists) isUnique = true
+        attempts++
+    }
+
+    if (!isUnique) {
+        return res.status(500).json({
+            message: "Account number generation failed, please retry"
+        })
+    }
 
     const account = await accountModel.create({
-    user:user._id,
-    accountNumber:generateAccountNumber()
+        user: user._id,
+        accountNumber: accountNumber
     })
 
     res.status(201).json({
         account
     })
-
 }
 
-async function getUserAccountsController(req,res){
+async function getUserAccountsController(req, res) {
 
     const accounts = await accountModel.find({
         user: req.user._id
@@ -27,34 +44,31 @@ async function getUserAccountsController(req,res){
     });
 }
 
-async function getAccountBalanceController(req,res) {
+async function getAccountBalanceController(req, res) {
 
-    const {accountId } = req.params;
+    const { accountId } = req.params;
 
-    const account= await accountModel.findOne({
-        _id:accountId,
-        user:req.user._id
+    const account = await accountModel.findOne({
+        _id: accountId,
+        user: req.user._id
     })
-    if(!account){
+
+    if (!account) {
         return res.status(404).json({
-            message:"Account not Found "
+            message: "Account not Found"
         })
     }
 
-    const balance= await account.getBalance()
+    const balance = await account.getBalance()
 
     res.status(200).json({
-    accountId: account._id,
-    balance: balance
+        accountId: account._id,
+        balance: balance
     })
-    
 }
 
-
-
-module.exports={
+module.exports = {
     createAccountController,
     getUserAccountsController,
     getAccountBalanceController
-
 }
